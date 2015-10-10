046396d2f5b41d29816226cba950a1593a3a3a9f
cletcher <charlesletcher@gmail.com>
2015-10-10 11:30:22 -0500

    more changes

--- Charles/cwltest1/cwltest1/ViewController.swift
+++ Charles/cwltest1/cwltest1/ViewController.swift
@@ -7,8 +7,24 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var likeButton: UIButton?
+    @IBOutlet weak var instruction: UILabel?
+ 
+    //http://stackoverflow.com/questions/24043904/creating-and-playing-a-sound-in-swift
+    @IBAction func likedThis(sender: UIButton) {
+        instruction?.text="test";
+        
+        if let soundURL = NSBundle.mainBundle().URLForResource("Pew_Pew", withExtension: "wav") {
+            var mySound: SystemSoundID = 0
+            AudioServicesCreateSystemSoundID(soundURL, &mySound)
+            // Play
+            AudioServicesPlaySystemSound(mySound);
+        }
+    }
+    @IBOutlet weak var mybutt: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
